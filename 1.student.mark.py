7cdd3990715312b54cf95facba59b1f56daec425
Dak <khoida2410480@usth.edu.vn>
2025-11-29 10:48:07 +0700

    Practice 1 completed

---
+++ 1.student.mark.py
@@ -0,0 +1,49 @@
+student = []
+course = []
+mark ={}
+
+SCount = int(input("No of students: "))
+for i in range(SCount):
+    id = str(input("Student id: "))
+    name = input("Student name: ")
+    dob = input ("Student dob: ")
+    student.append({"ID": id, "Name": name, "DoB": dob})
+
+CCount = int(input("No of course: "))
+for i in range(CCount):
+    id = str(input("Course id: "))
+    name = input("Course name: ")
+    course.append({"ID": id, "Name": name})
+
+while True:
+    cmd = str(input("Input command (c_list, s_list, input_mark, show_mark, stop): "))
+
+    if cmd == "input_mark":
+        CId = input("Course id to input marks: ")
+        mark["CId"] = {}
+        for s in student:
+            mk = float(input(f"{s['ID']}_{s['Name']}'s mark: "))
+            mark['CId'][s['ID']] = mk
+
+    if cmd == "c_list":
+        for i in course:
+            print(i['ID'], i['Name'])
+
+    if cmd == "s_list":
+        for i in student:
+            print(f"{i['ID']}_{i['Name']}")
+
+    if cmd == "show_mark":
+        CId = input("Course id to show marks: ")
+        for i in student:
+            SId = s['ID']
+            if SId in mark["CId"]:
+                print(s['ID'], mark["CId"][SId])
+
+    if cmd == "stop":
+        print("Shutting down...")
+        break
+    
+
+    
+
